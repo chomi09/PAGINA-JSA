88c4cf5f8e2375cee6078de438e7748a170c6402
chomi09 <oscowissarx@email.com>
2026-05-25 16:31:40 -0500

    mis avances

--- PYTHON/numbers.py
+++
@@ -1,8 +0,0 @@
-print(type(9))
-print(type(23.87))
-print(2**4)
-print(3//2)
-print(10%3)
-age = input("insert you age: ")
-new_age = int(age) + 5
-print (new_age)
--- PYTHON/set.py
+++
@@ -1,7 +0,0 @@
-colors = {'red', 'green', 'blue'}
-# print (type(colors))
-# print('ble' is colors)
-# colors.add('violet')
-# print(colors)
-colors.clear()
-print(colors)
